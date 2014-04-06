054dc57fddb31290b0fa5fd0e94d5767000cba5a
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-04-05 22:42:29 -0700

    Add sustain option.

--- app/assets/javascripts/tones.js
+++ app/assets/javascripts/tones.js
@@ -8,3 +8,10 @@ oscillator.noteOn(0)
 $(document).on('keydown', function(event) {
   play(oscillator, event.keyCode)
 })
+
+// if the sustain checkbox is not checked, i.e. no sustain
+if (!$('#sustain').is(":checked")) {
+  $(document).on('keyup', function(event) {
+    oscillator.frequency.value = 0
+  })
+}
